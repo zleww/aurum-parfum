0c2136b729ebfd0acb44069ae755a4a7bc05dc39
Kyoko <ljennyjoy54@gmail.com>
2026-08-23 19:03:18 +0800

    commit changes in app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Replace this with your new Vercel deployment URL once deployed
-const API_URL = "https://your-new-aurum-service.vercel.app";
+const API_URL = "https://aurum-parfum.vercel.app";
 
 document.addEventListener("DOMContentLoaded", () => {
   loadAllPerfumes();
